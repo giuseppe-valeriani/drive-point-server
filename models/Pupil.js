ae405de20e0df87e9ce35724f816478f6f8ba0ea
Giuseppe Valeriani <giuggey85@gmail.com>
2024-03-11 20:58:25 +0000

    complete data structure, to be tested

--- models/Pupil.js
+++ models/Pupil.js
@@ -1,34 +1,22 @@
 const mongoose = require("mongoose");
 
-const Skills = new mongoose.Schema({
-  intro_1: {
-    type: Number,
-    required: true,
-    default: 0,
-  },
-  intro_2: {
-    type: Number,
-    required: true,
-    default: 0,
-  },
-  intro_3: {
-    type: Number,
-    required: true,
-    default: 0,
-  },
+const skillSchema = new mongoose.Schema({
+  skill: String,
+  value: Number,
+});
+
+const paymentSchema = new mongoose.Schema({
+  date: String,
+  amount: Number,
+  paid: Boolean,
 });
 
 const pupilsSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  starting_date: {
-    type: String,
-    required: true,
-  },
-  notes: { type: String, default: "" },
-  skills: { type: [Skills] },
+  name: String,
+  starting_date: String,
+  notes: String,
+  skills: [skillSchema],
+  payments: [paymentSchema],
 });
 
 module.exports = mongoose.model("Pupil", pupilsSchema);
