232bd271d5c7511d7e3d3d89b151be2ba03d39af
Giuseppe Valeriani <giuggey85@gmail.com>
2024-03-18 14:17:32 +0000

    add partials skills

--- models/StartingSkills.js
+++ models/StartingSkills.js
@@ -1,6 +1,32 @@
 const StartingSkills = [
-  { label: "Introduction 1", skill: "intro_1", value: 0 },
-  { label: "Introduction 2", skill: "intro_2", value: 0 },
+  // Beginners
+  { label: "Cockpit Drill", skill: "cockpit_drill", value: 0 },
+  { label: "Pedals", skill: "pedals", value: 0 },
+  { label: "Parking Strike", skill: "parking_strike", value: 0 },
+  { label: "Steering", skill: "steering", value: 0 },
+  { label: "Controls", skill: "controls", value: 0 },
+  { label: "Observations", skill: "observations", value: 0 },
+  { label: "Msm", skill: "msm", value: 0 },
+  { label: "Speed x Direction", skill: "speed_direction", value: 0 },
+  { label: "Timing of Signals", skill: "timing_signals", value: 0 },
+  // Moving Stopping
+  { label: "Moving Off", skill: "moving_off", value: 0 },
+  { label: "Pulling Over", skill: "pulling_over", value: 0 },
+  // others to be confirmed
+  // Manouvering
+  { label: "Park on Right x Reverse", skill: "park_right", value: 0 },
+  { label: "Reverse Bay Park", skill: "reverse_park", value: 0 },
+  { label: "Forward Bay Park", skill: "forward_park", value: 0 },
+  { label: "Parallel Park", skill: "parallel_park", value: 0 },
+  { label: "Emergency Stop", skill: "emergency_stop", value: 0 },
+  // On The Road
+  { label: "Road Positioning", skill: "road_positioning", value: 0 },
+  {
+    label: "Clearence x Obstructions",
+    skill: "clearence_obstructions",
+    value: 0,
+  },
+  { label: "Meeting Traffic", skill: "meeting_traffic", value: 0 },
 ];
 
 module.exports = StartingSkills;
